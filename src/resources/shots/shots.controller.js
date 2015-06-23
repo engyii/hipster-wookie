f55459b5755e6f6a7def4e93c459beec5abff1b4
Enguerrand Leclerc <engy@en.gy>
2015-06-23 18:41:20 +0200

    add cache retrieval

--- src/resources/shots/shots.controller.js
+++ src/resources/shots/shots.controller.js
@@ -3,37 +3,77 @@ var fs = require('fs');
 var path = require('path');
 var phantom = require('phantom');
 
+var resourceWait  = 300,
+    maxRenderWait = 10000;
+
 exports.index = function*(next) {
 	this.status = 200;
   this.body = [];
 };
 
 exports.shot = function*(next) {
-	var self = this;
+
+	var url = decodeURIComponent(this.params.sid);
 	var imgPath = path.resolve(global.imgRoot, this.params.sid + '.jpg');
-	console.log(this.params.sid);
-	console.log(imgPath);
-	 yield function(done) {
-		phantom.create(function (ph) {
-		  ph.createPage(function (page) {
-		    page.open(decodeURIComponent(self.params.sid), function (status) {
-		      console.log("opened? ", status);
-					page.render( imgPath, {format: 'jpeg', quality: '100'}, function() {
-						ph.exit();
-						done();
-					});
-		    });
-		  });
-		});
-	};
+
+
+	if (!fs.existsSync(imgPath)) {
+		yield function(done) {
+			lazyLoadPage(done, url, imgPath)
+		};
+	}
 
 	this.status = 200;
 	this.type = path.extname(imgPath);
 	this.body = fs.createReadStream(imgPath);
 };
 
-function stat(file) {
-  return function (done) {
-    fs.stat(file, done);
-  };
+function lazyLoadPage(doneCallback, url, imgPath) {
+
+	phantom.create(function (ph) {
+
+			ph.createPage(function (page) {
+
+			var doRender = function() {
+				page.render(imgPath, {format: 'jpeg', quality: '90'}, function() {
+					ph.exit();
+					doneCallback();
+				});
+			}
+
+			var count = 0, forcedRenderTimeout, renderTimeout;
+
+			page.set('viewportSize', { width: 1280, height : 1024 });
+			page.set('clipRect', { top: 0, left: 0, width: 1280, height: 1024 });
+
+			page.set('onResourceRequested', function (req) {
+				count += 1;
+				console.log('> ' + req.id + ' - ' + req.url);
+				clearTimeout(renderTimeout);
+			});
+
+			page.set('onResourceReceived', function (res) {
+				if (!res.stage || res.stage === 'end') {
+					count -= 1;
+					console.log(res.id + ' ' + res.status + ' - ' + res.url);
+					if (count === 0) {
+						renderTimeout = setTimeout(doRender, resourceWait);
+					}
+				}
+			});
+
+			page.open(url, function (status) {
+				if (status !== "success") {
+					console.log('Unable to load url', url);
+					ph.exit();
+					doneCallback();
+				} else {
+					forcedRenderTimeout = setTimeout(function () {
+	          console.log(count);
+	          doRender();
+	        }, maxRenderWait);
+				}
+			});
+		});
+	});
 }
